56f84e1892550c5f9a8cdd18c941316623361e65
chendongxu <10879234+donx_cdx@user.noreply.gitee.com>
2026-05-14 16:36:27 +0800

    功能完善

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
 import { useTheme } from './composables/useTheme'
-
-useTheme()
-
 import Header from './components/Header.vue'
 import Hero from './components/Hero.vue'
 import About from './components/About.vue'
 import Projects from './components/Projects.vue'
 import Contact from './components/Contact.vue'
 import Footer from './components/Footer.vue'
+
+useTheme()
 </script>
 
 <template>
